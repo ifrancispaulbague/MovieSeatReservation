4f63f92a04b2a8e85396f0743fe3dfd283aa99d5
jmacj <jm.nz0723@gmail.com>
2016-10-26 18:39:29 +0800

    Reservation Panel Update
    Whoooo kapagod :sake:

--- TicketReservation/Forms/reservations.cs
+++ TicketReservation/Forms/reservations.cs
@@ -339,6 +339,7 @@ namespace TicketReservation
 
         private void btnReserve_Click(object sender, EventArgs e)
         {
+            //A1-A10 1-10
             if (checkBox1.Checked && checkBox1.Enabled==true)
             {
                 systemDB.InsertSeat("A1", movies.mappingID);
@@ -387,7 +388,211 @@ namespace TicketReservation
             if (checkBox10.Checked && checkBox10.Enabled == true)
             {
                 systemDB.InsertSeat("A10", movies.mappingID);
-                checkBox5.Enabled = false;
+                checkBox10.Enabled = false;
+            }
+            //B1-B10 11-20
+            if (checkBox11.Checked && checkBox11.Enabled == true)
+            {
+                systemDB.InsertSeat("B1", movies.mappingID);
+                checkBox11.Enabled = false;
+            }
+            if (checkBox12.Checked && checkBox12.Enabled == true)
+            {
+                systemDB.InsertSeat("B2", movies.mappingID);
+                checkBox12.Enabled = false;
+            }
+            if (checkBox13.Checked && checkBox13.Enabled == true)
+            {
+                systemDB.InsertSeat("B3", movies.mappingID);
+                checkBox13.Enabled = false;
+            }
+            if (checkBox14.Checked && checkBox14.Enabled == true)
+            {
+                systemDB.InsertSeat("B4", movies.mappingID);
+                checkBox14.Enabled = false;
+            }
+            if (checkBox15.Checked && checkBox15.Enabled == true)
+            {
+                systemDB.InsertSeat("B5", movies.mappingID);
+                checkBox15.Enabled = false;
+            }
+            if (checkBox16.Checked && checkBox16.Enabled == true)
+            {
+                systemDB.InsertSeat("B6", movies.mappingID);
+                checkBox16.Enabled = false;
+            }
+            if (checkBox17.Checked && checkBox17.Enabled == true)
+            {
+                systemDB.InsertSeat("B7", movies.mappingID);
+                checkBox17.Enabled = false;
+            }
+            if (checkBox18.Checked && checkBox18.Enabled == true)
+            {
+                systemDB.InsertSeat("B8", movies.mappingID);
+                checkBox18.Enabled = false;
+            }
+            if (checkBox19.Checked && checkBox19.Enabled == true)
+            {
+                systemDB.InsertSeat("B9", movies.mappingID);
+                checkBox19.Enabled = false;
+            }
+            if (checkBox20.Checked && checkBox20.Enabled == true)
+            {
+                systemDB.InsertSeat("B10", movies.mappingID);
+                checkBox20.Enabled = false;
+            }
+            //C1-C10 21-30
+            if (checkBox21.Checked && checkBox21.Enabled == true)
+            {
+                systemDB.InsertSeat("C1", movies.mappingID);
+                checkBox21.Enabled = false;
+            }
+            if (checkBox22.Checked && checkBox22.Enabled == true)
+            {
+                systemDB.InsertSeat("C2", movies.mappingID);
+                checkBox22.Enabled = false;
+            }
+            if (checkBox23.Checked && checkBox23.Enabled == true)
+            {
+                systemDB.InsertSeat("C3", movies.mappingID);
+                checkBox23.Enabled = false;
+            }
+            if (checkBox24.Checked && checkBox24.Enabled == true)
+            {
+                systemDB.InsertSeat("C4", movies.mappingID);
+                checkBox24.Enabled = false;
+            }
+            if (checkBox25.Checked && checkBox25.Enabled == true)
+            {
+                systemDB.InsertSeat("C5", movies.mappingID);
+                checkBox25.Enabled = false;
+            }
+            if (checkBox26.Checked && checkBox26.Enabled == true)
+            {
+                systemDB.InsertSeat("C6", movies.mappingID);
+                checkBox26.Enabled = false;
+            }
+            if (checkBox27.Checked && checkBox27.Enabled == true)
+            {
+                systemDB.InsertSeat("C7", movies.mappingID);
+                checkBox27.Enabled = false;
+            }
+            if (checkBox28.Checked && checkBox28.Enabled == true)
+            {
+                systemDB.InsertSeat("C8", movies.mappingID);
+                checkBox28.Enabled = false;
+            }
+            if (checkBox29.Checked && checkBox29.Enabled == true)
+            {
+                systemDB.InsertSeat("C9", movies.mappingID);
+                checkBox29.Enabled = false;
+            }
+            if (checkBox30.Checked && checkBox30.Enabled == true)
+            {
+                systemDB.InsertSeat("C10", movies.mappingID);
+                checkBox30.Enabled = false;
+            }
+            //D1-D10 31-40
+            if (checkBox31.Checked && checkBox31.Enabled == true)
+            {
+                systemDB.InsertSeat("D1", movies.mappingID);
+                checkBox31.Enabled = false;
+            }
+            if (checkBox32.Checked && checkBox32.Enabled == true)
+            {
+                systemDB.InsertSeat("D2", movies.mappingID);
+                checkBox32.Enabled = false;
+            }
+            if (checkBox33.Checked && checkBox33.Enabled == true)
+            {
+                systemDB.InsertSeat("D3", movies.mappingID);
+                checkBox33.Enabled = false;
+            }
+            if (checkBox34.Checked && checkBox34.Enabled == true)
+            {
+                systemDB.InsertSeat("D4", movies.mappingID);
+                checkBox34.Enabled = false;
+            }
+            if (checkBox35.Checked && checkBox35.Enabled == true)
+            {
+                systemDB.InsertSeat("D5", movies.mappingID);
+                checkBox35.Enabled = false;
+            }
+            if (checkBox36.Checked && checkBox36.Enabled == true)
+            {
+                systemDB.InsertSeat("D6", movies.mappingID);
+                checkBox36.Enabled = false;
+            }
+            if (checkBox37.Checked && checkBox37.Enabled == true)
+            {
+                systemDB.InsertSeat("D7", movies.mappingID);
+                checkBox37.Enabled = false;
+            }
+            if (checkBox38.Checked && checkBox38.Enabled == true)
+            {
+                systemDB.InsertSeat("D8", movies.mappingID);
+                checkBox38.Enabled = false;
+            }
+            if (checkBox39.Checked && checkBox39.Enabled == true)
+            {
+                systemDB.InsertSeat("D9", movies.mappingID);
+                checkBox39.Enabled = false;
+            }
+            if (checkBox40.Checked && checkBox40.Enabled == true)
+            {
+                systemDB.InsertSeat("D10", movies.mappingID);
+                checkBox40.Enabled = false;
+            }
+            //E1-E10 41-50
+            if (checkBox41.Checked && checkBox41.Enabled == true)
+            {
+                systemDB.InsertSeat("E1", movies.mappingID);
+                checkBox41.Enabled = false;
+            }
+            if (checkBox42.Checked && checkBox42.Enabled == true)
+            {
+                systemDB.InsertSeat("E2", movies.mappingID);
+                checkBox42.Enabled = false;
+            }
+            if (checkBox43.Checked && checkBox43.Enabled == true)
+            {
+                systemDB.InsertSeat("E3", movies.mappingID);
+                checkBox43.Enabled = false;
+            }
+            if (checkBox44.Checked && checkBox44.Enabled == true)
+            {
+                systemDB.InsertSeat("E4", movies.mappingID);
+                checkBox44.Enabled = false;
+            }
+            if (checkBox45.Checked && checkBox45.Enabled == true)
+            {
+                systemDB.InsertSeat("E5", movies.mappingID);
+                checkBox45.Enabled = false;
+            }
+            if (checkBox46.Checked && checkBox46.Enabled == true)
+            {
+                systemDB.InsertSeat("E6", movies.mappingID);
+                checkBox46.Enabled = false;
+            }
+            if (checkBox47.Checked && checkBox47.Enabled == true)
+            {
+                systemDB.InsertSeat("E7", movies.mappingID);
+                checkBox47.Enabled = false;
+            }
+            if (checkBox48.Checked && checkBox48.Enabled == true)
+            {
+                systemDB.InsertSeat("E8", movies.mappingID);
+                checkBox48.Enabled = false;
+            }
+            if (checkBox49.Checked && checkBox49.Enabled == true)
+            {
+                systemDB.InsertSeat("E9", movies.mappingID);
+                checkBox49.Enabled = false;
+            }
+            if (checkBox50.Checked && checkBox50.Enabled == true)
+            {
+                systemDB.InsertSeat("E10", movies.mappingID);
+                checkBox50.Enabled = false;
             }
         }
     }
